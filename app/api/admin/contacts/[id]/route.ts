7cfaefc74fd8d1d8ec6df6067617fa881304cdfc
jagwanrahul7-maker <jagwanrahul7@gmail.com>
2025-12-09 16:11:09 +0530

    fixed [id] route.ts code

--- app/api/admin/contacts/[id]/route.ts
+++ app/api/admin/contacts/[id]/route.ts
@@ -3,10 +3,10 @@ import { prisma } from "@/lib/prisma";
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } },
+  context: { params: { id: string } },
 ) {
   try {
-    const { id } = params;
+    const id = context.params.id;
 
     if (!id) {
       return NextResponse.json(
@@ -15,7 +15,6 @@ export async function DELETE(
       );
     }
 
-    
     const contact = await prisma.contact.findUnique({
       where: { id },
     });
